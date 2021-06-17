5e01e5e7207fdabe096b8c551c1438861d63ad0f
Vishal Hasija <vishal.hasija1996@gmail.com>
2021-06-18 03:02:18 +0530

    2 Handler Functions added. 1 Func returns a response while other read the request body and return the same data to client.

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
 func main() {
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		d, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Request not supported", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(rw, "Hello %s", d)
+	})
+	http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(rw, "Goodbye")
+	})
+
 	http.ListenAndServe(":8080", nil)
 }
